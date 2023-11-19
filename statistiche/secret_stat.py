e77927718da4795512220c226f19bed4541ea201
Xienaa <marco.mulas@edu.itspiemonte.it>
2023-11-19 17:26:20 +0100

    aggiornato .gitingore

--- statistiche/secret_stat.py
+++ statistiche/secret_stat.py
@@ -5,9 +5,34 @@ from pydantic import BaseModel
 
 conn = sqlite3.connect(r"../database/db.sqlite/shopping_trends.sqlite",check_same_thread=False)
 
-def calculate_average_age_from_db():
+def calcola_eta_media_da_db():
+
     cursor = conn.cursor()
     query = 'SELECT AVG("Age") FROM shopping_trends'
     cursor.execute(query)
-    average_age = cursor.fetchone()[0]
-    return average_age
+    eta_media = cursor.fetchone()[0]
+    # Arrotonda l'età media a tre cifre decimali
+    eta_media_arrotondata = round(eta_media, 3)
+    risultato = {
+        'eta_media': eta_media_arrotondata,
+        'messaggio': 'Calcolo dell\'età media avvenuto con successo.'
+    }
+    return risultato
+
+
+def calcola_spesa_media_da_db():
+    cursor = conn.cursor()
+    query = 'SELECT AVG("Purchase Amount") FROM shopping_trends'
+    cursor.execute(query)
+    spesa_media = cursor.fetchone()[0]
+    
+    print(f"Spesa media prima dell'arrotondamento: {spesa_media}")
+
+    spesa_media_arrotondata = round(spesa_media, 2)
+
+    risultato = {
+        'spesa_media': spesa_media_arrotondata,
+        'messaggio': 'Calcolo della spesa media avvenuto con successo.'
+    }
+
+    return risultato
--- statistiche/stat_api.py
+++ statistiche/stat_api.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-from secret_stat import calculate_average_age_from_db
+from secret_stat import calcola_eta_media_da_db,calcola_spesa_media_da_db
 
 app = FastAPI()
 
 @app.get("/average_age")
 async def calculate_average_age_endpoint():
-    average_age = calculate_average_age_from_db()
+    average_age = calcola_eta_media_da_db()
     return {"average_age": average_age}
+
+@app.get("/importo_medio_acquisto")
+async def calcola_importo_medio_acquisto():
+    importo_medio_acquisto = calcola_spesa_media_da_db()
+    return {"importo_medio_acquisto": importo_medio_acquisto}
