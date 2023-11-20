90b8002a25a1fe8ac2637a32589c5dc40101925b
Xienaa <marco.mulas@edu.itspiemonte.it>
2023-11-20 15:27:06 +0100

    :bug: FEAT le API di statistica funzionanti

--- statistiche/secret_stat.py
+++ statistiche/secret_stat.py
@@ -72,3 +72,66 @@ async def calcola_mediana_previous_purchases():
     finally:
         # Chiudi la connessione al database alla fine
         conn_db.close()
+
+
+async def calcola_distribuzione_percentuale():
+    try:
+        # Apri la connessione al database
+        conn_db = apri_connessione_database()
+
+        # Esegui la query e carica i risultati in un DataFrame pandas
+        query = 'SELECT [Frequency of Purchases] FROM shopping_trends'
+        df = pd.read_sql_query(query, conn_db)
+
+        if not df.empty:
+            # Rimuovi spazi extra e converti tutto in minuscolo
+            cleaned_text = df['Frequency of Purchases'].str.strip().str.lower()
+
+            # Rimuovi eventuali spazi vuoti rimasti dopo la pulizia
+            cleaned_text = cleaned_text.replace('', pd.NA)
+
+            # Sostituisci "Every 3 Months" con un placeholder
+            cleaned_text = cleaned_text.str.replace('every 3 months', 'every_3_months')
+
+            # Sostituisci i numeri con un placeholder
+            cleaned_text = cleaned_text.str.replace(r'\b\d+\b', 'numberplaceholder', regex=True)
+
+            # Dividi il testo in parole
+            words = cleaned_text.str.split()
+
+            # Sostituisci il placeholder con il numero originale
+            words = words.apply(lambda w: [word.replace('numberplaceholder', '') for word in w])
+
+            # Unisci le parole in un unico elenco di parole
+            flattened_words = words.explode()
+
+            # Sostituisci il placeholder con la parola originale
+            flattened_words = flattened_words.replace('every_3_months', 'every 3 months')
+
+            # Calcola le frequenze delle parole
+            word_counts = flattened_words.value_counts()
+
+            # Calcola la distribuzione percentuale in termini percentuali
+            percentage_distribution = (word_counts / word_counts.sum()) * 100
+
+            # Rimuovi eventuali valori nulli dovuti a spazi vuoti
+            percentage_distribution = percentage_distribution.dropna()
+
+            # Arrotonda i valori a 3 numeri dopo la virgola
+            percentage_distribution = percentage_distribution.round(3)
+
+            # Aggiungi il simbolo % agli output
+            percentage_distribution_with_percent = percentage_distribution.apply(lambda x: f"{x}%")
+
+            risultato = {
+                'distribuzione_percentuale': percentage_distribution_with_percent.to_dict(),
+                'messaggio': 'Calcolo della distribuzione percentuale avvenuto con successo.'
+            }
+            return risultato
+        else:
+            raise HTTPException(status_code=404, detail="Nessun dato trovato nella colonna 'Frequency of Purchases'.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Errore durante il calcolo della distribuzione percentuale: {str(e)}")
+    finally:
+        # Chiudi la connessione al database alla fine
+        conn_db.close()
--- statistiche/stat_api.py
+++ statistiche/stat_api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from secret_stat import calcola_eta_media_da_db,calcola_spesa_media_da_db,calcola_mediana_previous_purchases
+from secret_stat import calcola_eta_media_da_db,calcola_spesa_media_da_db,calcola_mediana_previous_purchases,calcola_distribuzione_percentuale
 
 app = FastAPI()
 
@@ -17,3 +17,8 @@ async def calcola_importo_medio_acquisto():
 async def api_calcola_mediana_previous_purchases():
     mediana_previous_purchases = await calcola_mediana_previous_purchases()
     return {"mediana_previous_purchases": mediana_previous_purchases}
+
+@app.get("/calcola_distribuzione_percentuale")
+async def api_calcola_distribuzione_percentuale():
+    distribuzione_percentuale = await calcola_distribuzione_percentuale()
+    return distribuzione_percentuale
