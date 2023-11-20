abd32da581faa0b063d6481a23c8354e9fee9b45
Xienaa <marco.mulas@edu.itspiemonte.it>
2023-11-20 14:48:34 +0100

    :bug: FEAT mediana previous puchases

--- statistiche/secret_stat.py
+++ statistiche/secret_stat.py
@@ -1,24 +1,27 @@
 import sqlite3
 from fastapi import HTTPException
-from pydantic import BaseModel
+import pandas as pd
 
+conn = sqlite3.connect(r"../database/db.sqlite/shopping_trends.sqlite", check_same_thread=False)
 
-conn = sqlite3.connect(r"../database/db.sqlite/shopping_trends.sqlite",check_same_thread=False)
+def apri_connessione_database():
+    return sqlite3.connect(r"../database/db.sqlite/shopping_trends.sqlite", check_same_thread=False)
 
 def calcola_eta_media_da_db():
-
-    cursor = conn.cursor()
-    query = 'SELECT AVG("Age") FROM shopping_trends'
-    cursor.execute(query)
-    eta_media = cursor.fetchone()[0]
-    # Arrotonda l'età media a tre cifre decimali
-    eta_media_arrotondata = round(eta_media, 3)
-    risultato = {
-        'eta_media': eta_media_arrotondata,
-        'messaggio': 'Calcolo dell\'età media avvenuto con successo.'
-    }
-    return risultato
-
+    try:
+        cursor = conn.cursor()
+        query = 'SELECT AVG("Age") FROM shopping_trends'
+        cursor.execute(query)
+        eta_media = cursor.fetchone()[0]
+        # Arrotonda l'età media a tre cifre decimali
+        eta_media_arrotondata = round(eta_media, 3)
+        risultato = {
+            'eta_media': eta_media_arrotondata,
+            'messaggio': 'Calcolo dell\'età media avvenuto con successo.'
+        }
+        return risultato
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Errore durante il calcolo dell'età media: {str(e)}")
 
 def calcola_spesa_media_da_db():
     try:
@@ -40,3 +43,32 @@ def calcola_spesa_media_da_db():
             raise HTTPException(status_code=404, detail="Nessun dato trovato o spesa media uguale a zero nella colonna 'Purchase Amount'.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Errore durante il calcolo della media del 'Purchase Amount (USD)': {str(e)}")
+
+async def calcola_mediana_previous_purchases():
+    try:
+        # Apri la connessione al database
+        conn_db = apri_connessione_database()
+
+        # Esegui la query e carica i risultati in un DataFrame pandas
+        query = 'SELECT [Previous Purchases] FROM shopping_trends'
+        df = pd.read_sql_query(query, conn_db)
+
+        if not df.empty:
+            # Calcola la mediana dei valori nella colonna 'Previous Purchases'
+            mediana_previous_purchases = df['Previous Purchases'].median()
+
+            # Arrotonda la mediana a due cifre decimali
+            mediana_previous_purchases_arrotondata = round(mediana_previous_purchases, 2)
+
+            risultato = {
+                'mediana_previous_purchases': mediana_previous_purchases_arrotondata,
+                'messaggio': 'Calcolo della mediana di "Previous Purchases" avvenuto con successo.'
+            }
+            return risultato
+        else:
+            raise HTTPException(status_code=404, detail="Nessun dato trovato nella colonna 'Previous Purchases'.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Errore durante il calcolo della mediana di 'Previous Purchases': {str(e)}")
+    finally:
+        # Chiudi la connessione al database alla fine
+        conn_db.close()
--- statistiche/stat_api.py
+++ statistiche/stat_api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from secret_stat import calcola_eta_media_da_db,calcola_spesa_media_da_db
+from secret_stat import calcola_eta_media_da_db,calcola_spesa_media_da_db,calcola_mediana_previous_purchases
 
 app = FastAPI()
 
@@ -12,3 +12,8 @@ async def calculate_average_age_endpoint():
 async def calcola_importo_medio_acquisto():
     importo_medio_acquisto = calcola_spesa_media_da_db()
     return {"importo_medio_acquisto": importo_medio_acquisto} 
+
+@app.get("/calcola_mediana_previous_purchases")
+async def api_calcola_mediana_previous_purchases():
+    mediana_previous_purchases = await calcola_mediana_previous_purchases()
+    return {"mediana_previous_purchases": mediana_previous_purchases}
