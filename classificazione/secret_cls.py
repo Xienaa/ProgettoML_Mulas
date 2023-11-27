d280629b4952c8ada1ccaaec02f122910fb0fddd
Xienaa <marco.mulas@edu.itspiemonte.it>
2023-11-27 17:08:37 +0100

    aggiunto modello + modifiche varie al codce api

--- classificazione/secret_cls.py
+++ classificazione/secret_cls.py
@@ -1,50 +1,30 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+import torch
+from torchvision import models, transforms
 from PIL import Image
-import io
-import os
 
-app = FastAPI()
+# Carica il modello addestrato
+model = models.resnet50(pretrained=False)
+model.fc = torch.nn.Linear(2048, 13)  # Supponendo che il tuo modello abbia 13 classi
 
-# Carica il modello MobileNetV2 preaddestrato
-model = MobileNetV2(weights="imagenet")
+# Carica i pesi addestrati
+model.load_state_dict(torch.load('percorso/al/tuo/modello.pth'))
+model.eval()
 
-# Cartella in cui salvare le immagini di abbigliamento
-clothing_folder = "clothing_images"
+# Definisci la trasformazione dell'immagine
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
-# Crea la cartella se non esiste
-if not os.path.exists(clothing_folder):
-    os.makedirs(clothing_folder)
+# Funzione per prevedere la classe di un'immagine
+def predict_class(image_path):
+    image = Image.open(image_path).convert('RGB')
+    image = transform(image)
+    image = image.unsqueeze(0)  # Aggiungi la dimensione del batch
 
-# Funzione per classificare e salvare l'immagine
-def classifica_vestiti(file: UploadFile = File(...)):
-    # Leggi l'immagine come un oggetto PIL
-    content = file.file.read()
-    image = Image.open(io.BytesIO(content))
-
-    # Ridimensiona l'immagine a dimensioni compatibili con MobileNetV2
-    image = image.resize((224, 224))
-
-    # Preprocessa l'immagine per adattarla al modello
-    img_array = preprocess_input(np.array(image).reshape(1, 224, 224, 3))
-
-    # Ottieni la predizione del modello
-    predictions = model.predict(img_array)
-
-    # Decodifica le predizioni
-    decoded_predictions = decode_predictions(predictions)
-
-    # Estrai il nome della classe predetta
-    predicted_class = decoded_predictions[0][0][1]
-
-    # Salva l'immagine nella cartella appropriata se è un capo di abbigliamento
-    if "clothing" in predicted_class.lower():
-        image_path = os.path.join(clothing_folder, file.filename)
-        image.save(image_path)
-        # Restituisci una risposta JSON con il messaggio e la classe predetta
-        return JSONResponse(content={"message": "Immagine classificata come abbigliamento e salvata con successo", "predicted_class": predicted_class})
-    else:
-        # Restituisci una risposta JSON indicando che l'immagine non è un capo di abbigliamento
-        return JSONResponse(content={"message": "Immagine non classificata come abbigliamento", "predicted_class": predicted_class})
-
+    with torch.no_grad():
+        output = model(image)
+    
+    _, predicted_class = torch.max(output, 1)
+    return predicted_class.item()
